ff307e022214465323ab5768bb4c0534b234ec33
Sayantan Karmakar <sayantankarmakar@outlook.com>
2022-04-02 21:09:14 +0530

    setup prettier, eslint, husky

---
+++ ts/src/dex.ts
@@ -0,0 +1,14 @@
+import { PublicKey } from "@solana/web3.js";
+
+export class Dex {
+  public address: PublicKey;
+
+  constructor(address: PublicKey) {
+    this.address = address;
+  }
+
+  public hello_world(x: number): string {
+    var c = x + 10;
+    return `hello ${c}`;
+  }
+}
